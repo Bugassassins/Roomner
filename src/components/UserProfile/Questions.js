a6a9f43895313b2a5345c88018872dbcef3cfbe0
viditchopra1500 <vidit1500@gmail.com>
2020-11-29 13:11:56 +0530

    removed form bug

--- src/components/UserProfile/Questions.js
+++ src/components/UserProfile/Questions.js
@@ -4,6 +4,9 @@ import { Radio } from "./Radio";
 const Questions=(props) => {
     const [curChoice1,setCurChoice1]=useState(props.response[0]);
     const [curChoice2,setCurChoice2]=useState(props.response[1]);
+    const [curChoice3,setCurChoice3]=useState(props.response[2]);
+    if(curChoice3===0)
+        setCurChoice3(25);
     // console.log(curChoice1);
     // console.log(props.response);
     return (
@@ -35,13 +38,13 @@ const Questions=(props) => {
                     <br />
                     <h1 className="form-name">Importance</h1>
                     <div className="form-importance">
-                        <input id="importance1" type="radio" name={props.id + 2} key={0} value={25} onChange={e => props.onButtonChange(props.id, e.target.value, 2)} />
+                        <Radio value={25} id={props.id} ind={2} selected={curChoice3}  onChange={(id,val,ind) => {setCurChoice3(val);props.onButtonChange(id,val,ind)} }/>
                         <label htmlFor="importance1" >A little</label>
 
-                        <input id="importance2" type="radio" name={props.id + 2} key={1} value={50} onChange={e => props.onButtonChange(props.id, e.target.value, 2)} />
+                        <Radio value={50} id={props.id} ind={2} selected={curChoice3}  onChange={(id,val,ind) => {setCurChoice3(val);props.onButtonChange(id,val,ind)} }/>
                         <label htmlFor="importance2" >Somewhat</label>
                         
-                        <input id="importance3" type="radio" name={props.id + 2} key={2} value={75} onChange={e => props.onButtonChange(props.id, e.target.value, 2)} />
+                        <Radio value={75} id={props.id} ind={2} selected={curChoice3}  onChange={(id,val,ind) => {setCurChoice3(val);props.onButtonChange(id,val,ind)} }/>
                         <label htmlFor="importance3" >Very</label>
                     </div>
         </div>
