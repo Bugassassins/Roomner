93b07e09fc0ddc64624bbd146ce18eb337f5c839
shridhar-t <thakur.shridhar3093@gmail.com>
2020-12-05 12:35:48 +0530

    add admin privileges to API

--- Backend/firebaseApp.js
+++ Backend/firebaseApp.js
@@ -1,13 +1,20 @@
 require("dotenv").config();  
-const firebase = require('firebase');   
-firebase.initializeApp({
-    apiKey:process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain:process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ,
-    databaseURL:process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId:process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket:process.env.REACT_APP_FIREBASE_STORAGE_BUCKET ,
-    messagingSenderId:process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId:process.env.REACT_APP_FIREBASE_MESSAGING_APP_ID ,
-    measurementId:process.env.REACT_APP_FIREBASE_MESSAGING_MEASUREMENT_ID
-});
-module.exports=firebase;
+const admin = require("firebase-admin");
+
+const serviceAccount = {
+    "type": process.env.ADMIN_TYPE,
+    "project_id": process.env.ADMIN_PROJECT_ID,
+    "private_key_id": process.env.ADMIN_PRIVATE_KEY_ID,
+    "private_key": process.env.ADMIN_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": process.env.ADMIN_CLIENT_EMAIL,
+    "client_id": process.env.ADMIN_CLIENT_ID,
+    "auth_uri": process.env.ADMIN_AUTH_URI,
+    "token_uri": process.env.ADMIN_TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.ADMIN_AUTH_PROVIDER_x509_CERT_URL,
+    "client_x509_cert_url": process.env.ADMIN_CLIENT_x509_CERT_URL
+};
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: process.env.ADMIN_DATABASE_URL
+});  
+module.exports=admin;
